b59e4ccb339bd3c708d097b6351bb8a74f69f94c
Erin Stansbury <ekstansbury@gmail.com>
2020-03-29 16:57:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-  end 
+  end
 
   def print_courses
     self.make_courses
